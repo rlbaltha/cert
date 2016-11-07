a4dab404ff2ac8f0a6c489f03ffa3e1046b01cec
Ron Balthazor <rlbaltha@uga.edu>
2016-11-07 08:24:11 -0500

    feature bug fix

--- src/AppBundle/Controller/FeatureController.php
+++ src/AppBundle/Controller/FeatureController.php
@@ -53,7 +53,7 @@ class FeatureController extends Controller
             $em->persist($entity);
             $em->flush();
 
-            return $this->redirect($this->generateUrl('feature_show', array('id' => $entity->getId())));
+            return $this->redirect($this->generateUrl('feature'));
         }
 
         return array(
