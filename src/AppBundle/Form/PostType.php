7b06eb4839c106af9dc1245985ab6433e0db1eef
Ron Balthazor <rlbaltha@uga.edu>
2017-06-14 13:58:44 -0400

    Post and menu update

---
+++ src/AppBundle/Form/PostType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class PostType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('category', 'choice', array(
+                'required'=> true,
+                'multiple'=> false,
+                'label' => 'Category',
+                'choices'  => array(
+                    'New Student' => 'New Student',
+                    'Continuing Student' => 'Continuing Student',
+                    'Pre-Capstone' => 'Pre-Capstone',
+                    'Capstone' => 'Capstone',
+                    'Mentor' => 'Mentor',
+                    'Director' => 'Director',
+                ),
+                // *this line is important*
+                'choices_as_values' => true,
+                'expanded' => true,
+            ))
+            ->add('title','text', array('attr' => array('class' => 'text form-control'),))
+            ->add('body', 'ckeditor', array('config_name' => 'editor_page',))
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Post'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'appbundle_post';
+    }
+}
